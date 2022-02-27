83d186c850d0c3c2495a13eb14f8c5142bad2081
alingse <alingse@foxmail.com>
2022-02-27 20:50:41 +0800

    add user

--- im_user.go
+++ im_user.go
@@ -171,3 +171,84 @@ func (UserMuteParam) GetPath() string {
 type UserMuteResponse struct {
 	BasicResponese
 }
+
+func (im *IM) CreateUser(param *CreateUserParam) (*CreateUserResponse, error) {
+	var result CreateUserResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) UpdateUser(param *UpdateUserParam) (*UpdateUserResponse, error) {
+	var result UpdateUserResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) RefreshToken(param *RefreshTokenParam) (*RefreshTokenResponse, error) {
+	var result RefreshTokenResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) UserBlock(param *UserBlockParam) (*UserBlockResponse, error) {
+	var result UserBlockResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) UserUnBlock(param *UserUnBlockParam) (*UserUnBlockResponse, error) {
+	var result UserUnBlockResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) UpdateUserInfo(param *UpdateUserInfoParam) (*UpdateUserInfoResponse, error) {
+	var result UpdateUserInfoResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) GetUserInfos(param *GetUserInfosParam) (*GetUserInfosResponse, error) {
+	var result GetUserInfosResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) UserSetDonnop(param *UserSetDonnopParam) (*UserSetDonnopResponse, error) {
+	var result UserSetDonnopResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (im *IM) UserMute(param *UserMuteParam) (*UserMuteResponse, error) {
+	var result UserMuteResponse
+	err := im.PostFormAs(param, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
