0ffa9cb8a583148521e9dae49d39a25c3ec0f4c7
alingse <alingse@foxmail.com>
2022-11-15 00:07:43 +0800

    add api

--- gen/main.go
+++ gen/main.go
@@ -2,13 +2,14 @@ package main
 
 func main() {
 	/*
-		func (ChatRoomCreateParam) GetPath() string {
-			return PathChatRoomCreate
-		}
+		package yunxin
+		// go generate go run gen/main.go
+			func (ChatRoomCreateParam) GetPath() string {
+				return PathChatRoomCreate
+			}
 
-		func (im *IM) ChatRoomCreate(ctx context.Context, param *ChatRoomCreateParam) (*ChatRoomCreateResponse, error) {
-			return Request[ChatRoomCreateResponse](im.Client, ctx, param)
-		}
+			func (c *Client) ChatRoomCreate(ctx context.Context, param *ChatRoomCreateParam) (*ChatRoomCreateResponse, error) {
+				return Request[ChatRoomCreateResponse](c, ctx, param)
+			}
 	*/
-
 }
