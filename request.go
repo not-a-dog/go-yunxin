0c90c3ab329f5a1e6131e17cb346db9f7366194d
alingse <alingse@foxmail.com>
2022-11-16 23:02:54 +0800

    updat

--- request.go
+++ request.go
@@ -19,17 +19,16 @@ func RegisterEncoder(value interface{}, encoder func(reflect.Value) string) {
 	defaultEncoder.RegisterEncoder(value, encoder)
 }
 
-func init() {
-	RegisterEncoder(StringSlice{}, JSONFormEncode)
-	RegisterEncoder(AntispamCustom{}, JSONFormEncode)
-}
-
-type StringSlice []string
-
 func JSONFormEncode(v reflect.Value) string {
 	value := v.Interface()
 	data, _ := json.Marshal(value)
 	return string(data)
+}
+
+type StringSlice []string
+
+func init() {
+	RegisterEncoder(StringSlice{}, JSONFormEncode)
 }
 
 type Param interface {
