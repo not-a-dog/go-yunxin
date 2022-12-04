12efb9911ce65f419e3c83da05af0d49bd071df0
alingse <alingse@foxmail.com>
2022-12-05 00:18:00 +0800

    clean with Std logger

--- example/chatroom/main.go
+++ example/chatroom/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"log"
 	"os"
-	"strconv"
-	"time"
 
 	"github.com/not-a-dog/go-yunxin"
 )
@@ -18,39 +16,11 @@ func main() {
 	}
 
 	ctx := context.Background()
-	client := yunxin.NewClient(appKey, appSecret)
-	result, err := client.ChatroomGet(ctx, &yunxin.ChatroomGetParam{
-		RoomID:              2757903841,
-		NeedOnlineUserCount: true,
-	})
-	log.Printf("result %+v err %+v \n", result, err)
+	client := yunxin.NewClient(appKey, appSecret,
+		yunxin.WithLogger(yunxin.StdLogger()))
 
-	result2, err := client.ChatRoomRequestAddr(ctx, &yunxin.ChatRoomRequestAddrParam{
-		RoomID:     2757903841,
-		AccID:      "b845d05e8bf13b3be57e09095c42f577",
-		ClientType: yunxin.ClientTypeWeblink,
-	})
-	log.Printf("result2 %+v err %+v \n", result2, err)
-
-	result3, err := client.ChatRoomSendMsg(ctx, &yunxin.ChatRoomSendMsgParam{
-		RoomID:    2757903841,
-		FromAccID: "b845d05e8bf13b3be57e09095c42f577",
-		MsgID:     "test-chatroom-" + strconv.FormatInt(time.Now().UnixMicro(), 10),
-		Attach:    "hello",
-		MsgType:   yunxin.MsgTypeText,
-		SubType:   1,
-	})
-	log.Printf("result3 %+v err %+v \n", result3, err)
-
-	result4, err := client.ChatroomGetBatch(ctx, &yunxin.ChatroomGetBatchParam{
-		RoomIDs:             []string{"2757903841"},
-		NeedOnlineUserCount: true,
-	})
-	log.Printf("result4 %+v err %+v \n", result4, err)
-
-	result5, err := client.ChatRoomMembersByPage(ctx, &yunxin.ChatRoomMembersByPageParam{
+	_, _ = client.ChatRoomUpdateInOutNotification(ctx, &yunxin.ChatRoomUpdateInOutNotificationParam{
 		RoomID: 2757903841,
-		Type:   yunxin.ChatRoomMemberQueryTypeDefault,
+		Close:  true,
 	})
-	log.Printf("result5 %+v err %+v \n", result5, err)
 }
--- logger.go
+++ logger.go
@@ -1,5 +1,7 @@
 package yunxin
 
+import "log"
+
 type Logger interface {
 	Info(args ...interface{})
 	Error(args ...interface{})
@@ -11,3 +13,18 @@ var _ Logger = new(emptyLogger)
 
 func (e *emptyLogger) Info(args ...interface{})  {}
 func (e *emptyLogger) Error(args ...interface{}) {}
+
+type stdLogger struct{}
+
+var _ Logger = new(stdLogger)
+
+func (e *stdLogger) Info(args ...interface{}) {
+	log.Println(args...)
+}
+func (e *stdLogger) Error(args ...interface{}) {
+	log.Println(args...)
+}
+
+func StdLogger() Logger {
+	return &stdLogger{}
+}
